d98de5a9743c1cf67e7f4ee049705371df6234cc
KavithaVaishnav <kavithavaishnav456@hmail.com>
2016-10-20 17:44:36 +0530

    custom directive

---
+++ app/controllers/tableController.js
@@ -0,0 +1,10 @@
+app.controller('homeController', function ($scope) {
+
+     $scope.names = [
+                       {index: '1' ,name : 'user1' , id : '101' , designation : 'PAT'},
+                       {index: '2' ,name : 'user2' , id : '102' , designation : 'Manager'},
+                       {index: '3' ,name : 'user3' , id : '103' , designation : 'Senior associate'},
+                       {index: '4' ,name : 'user4' , id : '104' , designation : 'Team Leader'},
+                       {index: '5' ,name : 'user5' , id : '105' , designation : 'Analyst'}
+                     ]
+});
